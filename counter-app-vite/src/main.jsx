b0faf69dc3135cb9ae18d22ca946d7abe8635aee
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-18 18:23:04 -0300

    Deletes unnecessary files and creates a hello world

--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+
+function App(){
+    return (<h1>Hello World!</h1>);
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
 )
+
+

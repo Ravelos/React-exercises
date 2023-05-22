f8a60421c9a17a22a4ac4832df28ebdf1a30ebd8
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-22 17:39:01 -0300

    Tests getActiveUser function

--- counter-app-vite/test/base-test/05-functions.test.cjs
+++ counter-app-vite/test/base-test/05-functions.test.cjs
@@ -1,18 +1,22 @@
-import { getActiveUser, getUser } from "../../src/base-test/05-functions"
+import { getActiveUser, getUser } from "../../src/base-test/05-functions";
 
-describe('Test on 05-functions.js', () => { 
-    const testUser = {
-        uid: 'ABCD123',
-        userName: 'Lucho'
-    }
+describe("Test on 05-functions.js", () => {
+  const testUser = {
+    uid: "ABCD123",
+    userName: "Lucho",
+  };
 
-    const user = getUser();
+  const user = getUser();
 
-    expect(testUser).toEqual(user)
- })
+  expect(testUser).toEqual(user);
+});
 
- test('getUsuario must return an objetc with the name I am sending', () => {
-     const name = 'Orlando';
-     const userName = getActiveUser();
-     expect(userName).toEqual(name);
-    })
+test("getUsuario must return an objetc with the name I am sending", () => {
+  const name = "Orlando";
+  const activeUser = getActiveUser(name);
+  expect(activeUser).toEqual({
+    uid: "AFG563",
+    userName: name,
+  });
+});
+w

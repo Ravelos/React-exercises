06a3835c1a94b312bf9d0975f4895ff81ca51266
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-23 17:04:09 -0300

    Modifies the extension for this file

---
+++ counter-app-vite/test/base-test/05-functions.test.js
@@ -0,0 +1,21 @@
+import { getActiveUser, getUser } from "../../src/base-test/05-functions";
+
+describe("Test on 05-functions.js", () => {
+  const testUser = {
+    uid: "ABCD123",
+    userName: "Lucho",
+  };
+
+  const user = getUser();
+
+  expect(testUser).toEqual(user);
+});
+
+test("getUsuario must return an objetc with the name I am sending", () => {
+  const name = "Orlando";
+  const activeUser = getActiveUser(name);
+  expect(activeUser).toEqual({
+    uid: "AFG563",
+    userName: name,
+  });
+});

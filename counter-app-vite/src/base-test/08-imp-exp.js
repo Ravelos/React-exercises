a38f0766790825069d5da57852ac604294ee1914
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-23 17:01:49 -0300

    Creates the file to test

---
+++ counter-app-vite/src/base-test/08-imp-exp.js
@@ -0,0 +1,6 @@
+import  { heroes }  from "../data/heroes";
+
+export const getHeroesById = (id) => heroes.find((heroe) => heroe.id === id)
+
+export const getHeroesByOwner = (owner) => heroes.filter( (heroe) => heroe.owner === owner);
+

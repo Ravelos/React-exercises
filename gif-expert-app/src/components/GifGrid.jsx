6b0614dea48a460dafa6f5425d6ef9f1fd6702f1
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 20:34:41 -0300

    Imports getgifs function

--- gif-expert-app/src/components/GifGrid.jsx
+++ gif-expert-app/src/components/GifGrid.jsx
@@ -1,11 +1,13 @@
+import { getGifs } from '../helpers/GetGifs';
 
 
 export const GifGrid = ({ category }) => {
 
-
-  return (
-    <>
-        <h3>{category}</h3>    
-    </>
-  )
+    getGifs();
+    
+    return (
+        <>
+            <h3>{category}</h3>    
+        </>
+    )
 }

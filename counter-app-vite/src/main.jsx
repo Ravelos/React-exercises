b5ae5aa6b23d66208df17fb7144f246c1b83ac62
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-24 21:39:20 -0300

    Updates tha component to use CounterApp

--- counter-app-vite/src/main.jsx
+++ counter-app-vite/src/main.jsx
@@ -4,13 +4,13 @@ import ReactDOM from 'react-dom/client';
 
 import './styles.css'
 import { CounterApp } from './CounterApp';
-import { FirstApp } from './FirstApp';
+// import { FirstApp } from './FirstApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        {/* <CounterApp value={20}/> */}
-        <FirstApp title='This is a title'/>
+        <CounterApp value={100}/>
+        {/* <FirstApp title='This is a title'/> */}
     </React.StrictMode>
 )
 

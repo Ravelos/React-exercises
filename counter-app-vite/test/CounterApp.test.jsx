b957ec032fc5a30af55bac934d514689fccdbce7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 00:34:51 -0300

    Add three more tests

--- counter-app-vite/test/CounterApp.test.jsx
+++ counter-app-vite/test/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render,screen } from "@testing-library/react"
+import { fireEvent, render,screen } from "@testing-library/react"
 import { CounterApp } from "../src/CounterApp"
 
 describe('Testing CounterApp component', () => {
@@ -14,5 +14,28 @@ describe('Testing CounterApp component', () => {
         render(<CounterApp value={100}/>);
         expect( screen.getByText(100)).toBeTruthy();
     });
-      
+    
+    test('should increment with +1 button', () => { 
+        render(<CounterApp value={value}/>);
+        fireEvent.click( screen.getByText('+1') );
+        expect( screen.getByText('11')).toBeTruthy();
+    });
+
+    test('should increment with -1 button', () => { 
+        render(<CounterApp value={value}/>);
+        fireEvent.click( screen.getByText('-1') );
+        expect( screen.getByText('9')).toBeTruthy();
+    });
+
+    test('should reset the value', () => {
+        render(<CounterApp value={23}/>);
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        fireEvent.click( screen.getByText('+1') );
+        // fireEvent.click( screen.getByText('Reset') );
+
+        fireEvent.click( screen.getByRole('button', { name: 'btn-reset' }));
+        expect( screen.getByText(23)).toBeTruthy();
+    });
+
 });

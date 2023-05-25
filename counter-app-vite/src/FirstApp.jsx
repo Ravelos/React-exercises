0ff7cb108827b1a2e60acce063629c567da460dd
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-24 21:38:35 -0300

    Adds one more subtitle

--- counter-app-vite/src/FirstApp.jsx
+++ counter-app-vite/src/FirstApp.jsx
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types'
 export const FirstApp = ({title, subtitle}) => {
   return (
     <>
-        <h1>{title}</h1>
+        <h1 data-testid="test-title">{title}</h1>
+        <p>{subtitle}</p>
         <p>{subtitle}</p>
     </>
   )
@@ -11,7 +12,7 @@ export const FirstApp = ({title, subtitle}) => {
 
 FirstApp.propTypes = {
   title: PropTypes.string.isRequired,
-  subtitle: PropTypes.number
+  subtitle: PropTypes.string
 }
 
 FirstApp.defaultProps = {

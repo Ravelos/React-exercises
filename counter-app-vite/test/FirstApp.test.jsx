59866c611eea5bb0a4c5dc419038b3cc791ce343
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-24 21:40:21 -0300

    Comments unnecessary lines

--- counter-app-vite/test/FirstApp.test.jsx
+++ counter-app-vite/test/FirstApp.test.jsx
@@ -2,21 +2,30 @@ import { render } from "@testing-library/react"
 import { FirstApp } from "../src/FirstApp"
 
 describe('Testing FirstApp', () => { 
-    test('should match the snapshot', () => {
-        const title = 'Hello, it is me'
-        const { container } = render( <FirstApp title={title}/> );
+    // test('should match the snapshot', () => {
+    //     const title = 'Hello, it is me'
+    //     const { container } = render( <FirstApp title={title}/> );
 
-        expect(container).toMatchSnapshot()
-    })
+    //     expect(container).toMatchSnapshot()
+    // })
 
     test('should show the title in a h1 tag', () => {
         const title = 'Hello, it is me'
-        const { container, getByText } = render( <FirstApp title={title}/> );        
+        const { container, getByText, getByTestId } = render( <FirstApp title={title}/> );        
         expect( getByText(title) ).toBeTruthy();
 
-        const h1 = container.querySelector('h1');
-        expect( h1.innerHTML ).toContain( title );
+        // const h1 = container.querySelector('h1');
+        // expect( h1.innerHTML ).toContain( title );
+        expect( getByTestId('test-title').innerHTML ).toContain(title);
     })
+
+    test('should show the subtitle sent by props', () => {
+        const title = 'Hello, it is me';
+        const subtitle = 'I am a subtitle';
+        const { getAllByText } = render( <FirstApp title={title} subtitle={subtitle}/> );        
+        expect( getAllByText(subtitle) ).toHaveLength(2);  
+    })
+    
     
     
  })

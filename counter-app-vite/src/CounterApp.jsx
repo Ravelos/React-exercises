30a096a73fab677d48c35679046f5194035e142e
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 00:34:11 -0300

    Updates the component with aria-label

--- counter-app-vite/src/CounterApp.jsx
+++ counter-app-vite/src/CounterApp.jsx
@@ -26,7 +26,7 @@ export const CounterApp = ({value}) => {
         <h2>{counter}</h2> 
         <button onClick={ handleAdd }>+1</button>     
         <button onClick={ handleSubstract }>-1</button>     
-        <button onClick={ handleReset }>+Reset</button>     
+        <button aria-label='btn-reset' onClick={ handleReset }>Reset</button>     
     </>
   )
 }

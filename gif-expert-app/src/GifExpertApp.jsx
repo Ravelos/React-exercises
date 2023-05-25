db5bf81d4df9b9c87bbd4ca6ebd7b5cea989f045
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 14:48:56 -0300

    Uses useState to push a new element

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -1,7 +1,24 @@
+import { useState } from "react"
+import AddCategory from "./components/AddCategory"
+
 export const GifExpertApp = () => {
+
+    const [ categories, setCategories ] = useState( [ 'One Punch', 'Dragon Ball' ] )
+    
+    const onAddCategory = () => {
+        setCategories([ 'One Piece',...categories ])        
+    }
+
     return(
         <>
+            <AddCategory setCategories={ setCategories }/>
+
             <h1>GifExpertApp</h1>
+            <ol>
+                {categories.map( category =>{
+                    return <li key={ category }>{ category }</li>
+                })}
+            </ol>
         </>
-    )
+    );
 }

53f195146251bb19a75699736ac6c2aa203f2cb9
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 20:34:25 -0300

    Removes one category from state

--- gif-expert-app/src/GifExpertApp.jsx
+++ gif-expert-app/src/GifExpertApp.jsx
@@ -4,7 +4,7 @@ import { GifGrid } from "./components/GifGrid"
 
 export const GifExpertApp = () => {
 
-    const [ categories, setCategories ] = useState( [ 'One Punch', 'Dragon Ball' ] )
+    const [ categories, setCategories ] = useState( [ 'One Punch' ] )
     
     const onAddCategory = (NewCategory) => {
         if( categories.includes(NewCategory)) return;

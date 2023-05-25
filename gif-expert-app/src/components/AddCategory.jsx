319681c761e7d51515a0a4738b3e8af17a68b9d7
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-25 14:49:40 -0300

    Creates addCategory component

---
+++ gif-expert-app/src/components/AddCategory.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+
+const AddCategory = ({setCategories}) => {
+
+    const [inputValue, setInputValue] = useState('');
+
+    const onInputChange = ({target}) =>{
+        setInputValue(target.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        if(inputValue.trim().length <= 1) return; 
+        setCategories(categories => [ inputValue, ...categories ]);
+        setInputValue('');
+    }
+
+
+
+  return (
+
+    <form onSubmit={ onSubmit }>
+        <input
+            type="text"
+            placeholder="Look for gifs"
+            value={inputValue}
+            onChange={ onInputChange }
+        />
+    </form>
+  )
+}
+
+export default AddCategory

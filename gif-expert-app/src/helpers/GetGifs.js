df1b2a9e75892a3445418379be916f48778f50a1
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-05-29 22:40:28 -0300

    Removes console.log

--- gif-expert-app/src/helpers/GetGifs.js
+++ gif-expert-app/src/helpers/GetGifs.js
@@ -1,5 +1,5 @@
 export const getGifs = async(category) => {
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=8Z1qzXpXYEqIgNoTaqbR6McuMNUipsjO&q=${category}&limit=20`;
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=8Z1qzXpXYEqIgNoTaqbR6McuMNUipsjO&q=${category}&limit=10`;
 
     const resp = await fetch(url);
     const { data } = await resp.json();
@@ -8,8 +8,9 @@ export const getGifs = async(category) => {
         id: img.id,
         title: img.title,
         url: img.images.downsized_medium.url
-
     }));
+
+    console.log(gifs);
 
     return gifs;        
 };

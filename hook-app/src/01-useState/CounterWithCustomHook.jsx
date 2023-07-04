6474260a37ac7161ad8b1a4e0e52644d47a2bc52
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-04 18:42:25 -0300

    Adds the functions to handle on click

--- hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -1,20 +1,24 @@
-import { useCounter } from "../hooks/useCounter"
-
+import { useCounter } from "../hooks/useCounter";
 
 const CounterWithCustomHook = () => {
 
-    const{counter} = useCounter();
+  const { counter, increment, decrement, reset } = useCounter();
 
   return (
     <>
-        <h1>Counter with hook: {counter} </h1>
-        <hr />
-        <button className="btn btn-primary" >+1</button>
-        <button className="btn btn-secondary" >Reset</button>
-        <button className="btn btn-primary" >-1</button>
-      
+      <h1>Counter with hook: {counter} </h1>
+      <hr />
+      <button className="btn btn-primary" onClick={() => increment(2)}>
+        +1
+      </button>
+      <button className="btn btn-secondary" onClick={reset}>
+        Reset
+      </button>
+      <button className="btn btn-primary" onClick={() =>decrement(2)}>
+        -1
+      </button>
     </>
-  )
-}
+  );
+};
 
-export default CounterWithCustomHook
+export default CounterWithCustomHook;

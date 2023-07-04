17e4d2cb81f184a8c72a595154b1f87b9847ce08
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-04 18:01:29 -0300

    Uses CounterWithCustomHook

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp'
+// import { HooksApp } from './HooksApp'
+// import { CounterApp } from './01-useState/CounterApp'
+import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp/>
+    <CounterWithCustomHook/>
   </React.StrictMode>,
 )

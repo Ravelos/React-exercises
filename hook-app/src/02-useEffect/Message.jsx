88eb18a02914706c92b157443469c7aae12ad327
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-10 19:02:25 -0300

    Removes the listener

--- hook-app/src/02-useEffect/Message.jsx
+++ hook-app/src/02-useEffect/Message.jsx
@@ -1,18 +1,26 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 
 export const Message = () => {
+    const [coords, setCoords] = useState({x: 0,y: 0});
+
     useEffect(() => {
-      console.log('Message mounted');
+
+        const onMouseMove = ({x, y}) =>{
+            setCoords({ x, y })
+        }
+
+        window.addEventListener('mousemove', onMouseMove)
     
       return () => {
-        console.log('Message unmounted');
+        window.removeEventListener('mousemove', onMouseMove)
       }
     }, [])
     
   return (
     <>
         <h3>This user already exists</h3>
+        {JSON.stringify(coords)}
     </>
   )
 }

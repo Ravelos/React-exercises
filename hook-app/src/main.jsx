2a7fc5c19163dce5f211e69f406e9b0440d6aae0
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-10 18:17:38 -0300

    Implements simpleform component

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -3,10 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
-import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
+// import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
+import { SimpleForm } from './02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterWithCustomHook/>
+    <SimpleForm/>
   </React.StrictMode>,
 )

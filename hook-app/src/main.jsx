5d0b7c9693abbfff74267d926b63c39920ddb136
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-11 16:13:10 -0300

    Implements FormWithCustomHooks

--- hook-app/src/main.jsx
+++ hook-app/src/main.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { FormWithCustomHooks } from './02-useEffect/FormWithCustomHooks'
 // import { HooksApp } from './HooksApp'
 // import { CounterApp } from './01-useState/CounterApp'
 // import CounterWithCustomHook from './01-useState/CounterWithCustomHook'
-import { SimpleForm } from './02-useEffect/SimpleForm'
+// import { SimpleForm } from './02-useEffect/SimpleForm'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <SimpleForm/>
+    <FormWithCustomHooks />
   </React.StrictMode>,
 )

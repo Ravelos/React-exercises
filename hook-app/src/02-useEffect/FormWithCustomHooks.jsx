69b092ea753b90e092ae94b5c5cb97128a86616b
Oswaldo Ravelo <raveloswaldo@gmail.com>
2023-07-11 16:13:31 -0300

    Creates custom hooks

---
+++ hook-app/src/02-useEffect/FormWithCustomHooks.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from "react"
+import { useForm } from "../hooks/useForm";
+
+
+export const FormWithCustomHooks = () => {
+
+    const {formState, onInputChange, username, password, email} =useForm({
+        username:'',
+        email: '',
+        password:'',
+    });
+    
+    // const {username, email, password} = formState;
+
+    useEffect(() =>{
+        // console.log("UseEffect called")
+    },[]);
+
+    useEffect(() =>{
+        // console.log("formState called")
+    },[formState]);
+
+    useEffect(() =>{
+        // console.log("email called")
+    },[email]);
+
+  return (
+    <>
+        <h1>Form with custom hooks</h1>
+        <hr/>
+        <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            value={username}
+            onChange={onInputChange}
+            />
+        <input
+            type="email"
+            className="form-control mt-2"
+            placeholder="oswaldo@google.com"
+            name="email"
+            value={email}
+            onChange={onInputChange}
+        />
+        <input
+            type="password"
+            className="form-control mt-2"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onInputChange}
+        />
+    </>
+    
+  )
+}
